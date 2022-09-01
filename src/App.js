c00fdeb540e5314063854f8981b53446328d2734
Max Mandia <mmandia511@gmail.com>
2022-09-01 15:28:58 -0400

    first commit :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Logo from "./assets/logo.svg";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="content-container">
+        <img className="logo" alt="logo" src={Logo} />
+        <h2 id="desc">Photo sharing for Web3. Or whatever that is anyway.</h2>
+        <button>make friends</button>
+      </div>
     </div>
   );
 }
